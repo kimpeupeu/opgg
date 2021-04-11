143016287c3b5900aebd153b875ee9e6199d2808
Suyoung Kim <suyoung154@gmail.com>
2021-04-11 22:01:47 +0900

    검색 기록 디자인 수정

--- src/components/SummonerDetail/SearchBar/SearchHistory.tsx
+++ src/components/SummonerDetail/SearchBar/SearchHistory.tsx
@@ -27,24 +27,39 @@ const SearchHistoryItem: React.FC<SearchHistoryItemProps> = ({ name }) => {
 };
 
 const SearchHistory: React.FC<SearchHistoryProps> = ({ history }) => {
-  if (!history.length) {
-    return <p>검색 기록</p>;
-  }
-
   return (
-    <SearchHistoryWrap>
-      {history.map((name) => (
-        <SearchHistoryItem key={name} name={name} />
-      ))}
-    </SearchHistoryWrap>
+    <SearchHistoryWrapper>
+      <Title>검색기록</Title>
+      <SearchHistoryList>
+        {history.map((name) => (
+          <SearchHistoryItem key={name} name={name} />
+        ))}
+      </SearchHistoryList>
+    </SearchHistoryWrapper>
   );
 };
 
-const SearchHistoryWrap = styled.ol`
+const SearchHistoryWrapper = styled.div`
   display: flex;
   width: 100%;
   flex-direction: column;
   padding: 8px 12px;
+`;
+
+const Title = styled.h3`
+  font-size: 14px;
+  font-weight: 400;
+  margin: auto;
+  padding: 4px 0;
+  margin-bottom: 4px;
+  border-bottom: 1px solid #333;
+`;
+
+const SearchHistoryList = styled.ol`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  padding: 0;
 `;
 
 const SearchHistoryItemWrap = styled.li`
